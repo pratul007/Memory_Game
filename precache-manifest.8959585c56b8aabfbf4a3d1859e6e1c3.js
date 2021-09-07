86da18a7317781d55a50ad33cfa889f38e8e1d45
Pratul Choudhary <mailme.pratul14@gmail.com>
2021-09-07 14:09:02 +0530

    Updates

---
+++ precache-manifest.8959585c56b8aabfbf4a3d1859e6e1c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00dc9b0520d4eb476748",
+    "url": "/memory_game/static/css/main.9e35ef7e.chunk.css"
+  },
+  {
+    "revision": "00dc9b0520d4eb476748",
+    "url": "/memory_game/static/js/main.00dc9b05.chunk.js"
+  },
+  {
+    "revision": "924526445108d0feaaef",
+    "url": "/memory_game/static/js/1.92452644.chunk.js"
+  },
+  {
+    "revision": "25ef5a59cc76f30d5876",
+    "url": "/memory_game/static/js/runtime~main.25ef5a59.js"
+  },
+  {
+    "revision": "902763c6ed1632767b46f1f1b5c77311",
+    "url": "/memory_game/index.html"
+  }
+];
