a4c285da3ed41f8e9204986bfcbda25a489324a9
Pratul Choudhary <mailme.pratul14@gmail.com>
2021-09-07 13:18:42 +0530

    game

---
+++ src/App.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import Header from './components/header/Header';
+import Card from './components/card/Card';
+import GameOver from './components/card/GameOver';
+
+import './styles/main.css';
+
+class App extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     count: 0,
+  //   }
+  // }
+
+  state = { 
+    isFlipped: Array(16).fill(false),
+    shuffledCard: App.duplicateCard().sort(() => Math.random() - 0.5),
+    clickCount: 1,
+    prevSelectedCard: -1,
+    prevCardId: -1,
+    score: 0,
+    move: 0
+    
+  };
+
+  static duplicateCard = () => {
+    return ["A","B","C","D","E","F","G","H"].reduce((preValue, current, index, array) => {
+      return preValue.concat([current, current])
+    },[]);
+  };
+
+  handleClick = event => {
+    event.preventDefault();
+    const cardId = event.target.id;
+    const newFlipps = this.state.isFlipped.slice();
+    this.setState({
+        prevSelectedCard: this.state.shuffledCard[cardId],
+        prevCardId: cardId
+    });
+
+    if (newFlipps[cardId] === false) {
+      newFlipps[cardId] = !newFlipps[cardId];
+      this.setState(prevState => ({ 
+        isFlipped: newFlipps,
+        clickCount: this.state.clickCount + 1
+      }));
+
+      if (this.state.clickCount === 2) {
+        this.setState({ clickCount: 1 });
+        const prevCardId = this.state.prevCardId;
+        const newCard = this.state.shuffledCard[cardId];
+        const previousCard = this.state.prevSelectedCard;
+        this.state.move = this.state.move + 1;
+
+        this.isCardMatch(previousCard, newCard, prevCardId, cardId);
+      }
+    }
+  };
+
+  isCardMatch = (card1, card2, card1Id, card2Id) => {
+    if (card1 === card2) {
+      const hideCard = this.state.shuffledCard.slice();
+      hideCard[card1Id] = -1;
+      hideCard[card2Id] = -1;
+      this.state.score = this.state.score + 1;
+      setTimeout(() => {
+        this.setState(prevState => ({
+          shuffledCard: hideCard
+        }))
+      }, 1000);
+    } else {
+      const flipBack = this.state.isFlipped.slice();
+      flipBack[card1Id] = false;
+      flipBack[card2Id] = false;
+      setTimeout(() => {
+        this.setState(prevState => ({ isFlipped: flipBack }));
+      }, 1000);
+    }
+  };
+
+  restartGame = () => {
+    this.setState({
+      isFlipped: Array(16).fill(false),
+      shuffledCard: App.duplicateCard().sort(() => Math.random() - 0.5),
+      clickCount: 1,
+      prevSelectedCard: -1,
+      prevCardId: -1,
+      score:0,
+      move: 0
+    });
+  };
+
+  isGameOver = () => {
+    return this.state.isFlipped.every((element, index, array) => element !== false);
+  };
+
+  render() {
+    return (
+     <div>
+       <Header restartGame={this.restartGame} />
+       <p style={{fontWeight: "bold", fontFamily: 'Roboto'}}>Score: {this.state.score}</p>
+       <p style={{fontWeight: "bold", fontFamily: 'Roboto'}}>Move: {this.state.move}</p>
+       { this.isGameOver() ? <GameOver restartGame={this.restartGame} /> :
+       <div className="grid-container">
+          {
+            this.state.shuffledCard.map((cardNumber, index) => 
+              <Card
+                key={index} 
+                id={index} 
+                cardNumber={cardNumber} 
+                isFlipped={this.state.isFlipped[index]} 
+                handleClick={this.handleClick}     
+              />
+            )
+          }
+        </div>
+       }
+     </div>
+    );
+  }
+}
+
+export default App;
